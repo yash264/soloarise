9ef797c41848eb99cf1ba61fc025e8110b4a8017
yash264 <yp20222068@gmail.com>
2025-04-19 11:44:48 +0530

    Make routes for Pages

---
+++ client/src/Pages/Profile.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../Components/Navbar";
+
+function Profile() {
+    return (
+        <>
+            <Navbar />
+        </>
+    )
+}
+
+export default Profile;
---
+++ client/src/Pages/Ranking.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../Components/Navbar";
+
+function Ranking() {
+    return (
+        <>
+            <Navbar />
+        </>
+    )
+}
+
+export default Ranking;
